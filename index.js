cca1c349bbf4dca53871c91665f20ad1600e1e70
Justin Chu <developer@justinchu.com>
2022-06-01 10:43:09 -0700

    Add delta arrows

--- index.js
+++ index.js
@@ -8,15 +8,14 @@ const client = new Discord.Client();
 
 client.login(config.BOT_TOKEN);
 
-var job = new CronJob('0 12 * * *', function () {
+var job = new CronJob('0 15 * * *', function () {
   (async () => {
-
+    GrabGasPrediction();  
   })();
 }, null, true, config.TIMEZONE);
 
 client.on('ready', () => {  
-  job.start();
-  GrabGasPrediction();    
+  job.start();  
 });
 
 async function GrabGasPrediction(){
@@ -24,18 +23,27 @@ async function GrabGasPrediction(){
   let apiURL = config.Gas_Price_Page;
   let feed = await axios.get(apiURL);  
   let root = parse(feed.data.content);
+  let arrow;
+  if(feed.data.content.includes(".up-arrow{display:block;")){
+    arrow = "https://raw.githubusercontent.com/jaychu/gas-prices-bot/master/assets/up.JPG";
+  }else if(feed.data.content.includes(".down-arrow{display:block;")){
+    arrow = "https://raw.githubusercontent.com/jaychu/gas-prices-bot/master/assets/down.JPG";
+  }else{
+    arrow = "";
+  }
+  
   var value = root.querySelector('.data-box-change').removeWhitespace().text.toString();
   var msg = root.querySelector('.data-box-change').nextElementSibling.text.toString();
-  channel.send(PostMessage(value,msg));
+  channel.send(PostMessage(value,msg,arrow));
 }
 
-
-
-
-function PostMessage(value,msg){
+function PostMessage(value,msg,arrow){
   var message = new Discord.MessageEmbed({
     title:"Gas Price Delta: "+value,
     url:"https://toronto.citynews.ca/toronto-gta-gas-prices/",
+    thumbnail:{
+      url:arrow
+    },
     description:msg
   });
   return message;
