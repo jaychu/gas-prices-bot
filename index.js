585354586efb3b28a297572110e7df7346b4e0e2
Justin Chu <developer@justinchu.com>
2024-07-19 19:46:49 -0400

    Loop through gas-price channels rather

--- index.js
+++ index.js
@@ -10,35 +10,35 @@ discordClient.login(config.BOT_TOKEN);
 
 var discordJob = new CronJob('0 15 * * *', function () {
   (async () => {
-    GrabGasPrediction();  
-    
+    discordClient.channels.cache.each(
+      (channel) => GrabGasPrediction(channel)
+    )    
   })();
 }, null, true, config.TIMEZONE);
 
 discordClient.on('ready', () => {  
   discordJob.start();  
-  console.log("Yup");
 });
 
-async function GrabGasPrediction(){
-  const channel = discordClient.channels.cache.find(channel => channel.name === config.CHANNEL_NAME)
-  let apiURL = config.Gas_Price_Page;
-  let feed = await axios.get(apiURL);  
-  let root = parse(feed.data.content);
-  let arrow;
-  var value = root.querySelector('.data-box-change').removeWhitespace().text.toString();
-  var msg = root.querySelector('.data-box-change').parentNode.removeChild(root.querySelector('.data-box-change')).text.toString();
-  if(feed.data.content.includes("class=\"up-arrow\" style=\"display:block\"")){
-    arrow = "https://raw.githubusercontent.com/jaychu/gas-prices-bot/master/assets/up.JPG";
-    value = " + "+value;
-  }else if(feed.data.content.includes("class=\"down-arrow\" style=\"display:block\"")){
-    arrow = "https://raw.githubusercontent.com/jaychu/gas-prices-bot/master/assets/down.JPG";
-    value = " - "+value;
-  }else{
-    arrow = "";
+async function GrabGasPrediction(channel){
+  if(channel.name === config.CHANNEL_NAME){
+    let apiURL = config.Gas_Price_Page;
+    let feed = await axios.get(apiURL);  
+    let root = parse(feed.data.content);
+    let arrow;
+    var value = root.querySelector('.data-box-change').removeWhitespace().text.toString();
+    var msg = root.querySelector('.data-box-change').parentNode.removeChild(root.querySelector('.data-box-change')).text.toString();
+    if(feed.data.content.includes("class=\"up-arrow\" style=\"display:block\"")){
+      arrow = "https://raw.githubusercontent.com/jaychu/gas-prices-bot/master/assets/up.JPG";
+      value = " + "+value;
+    }else if(feed.data.content.includes("class=\"down-arrow\" style=\"display:block\"")){
+      arrow = "https://raw.githubusercontent.com/jaychu/gas-prices-bot/master/assets/down.JPG";
+      value = " - "+value;
+    }else{
+      arrow = "";
+    }    
+    channel.send(PostMessage(value,msg,arrow));
   }
-  
-  channel.send(PostMessage(value,msg,arrow));
 }
 
 function PostMessage(value,msg,arrow){
